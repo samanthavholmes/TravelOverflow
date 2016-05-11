adb517b1994d765f1120667ad238055574e99888
Evan Druce <drucee@gmail.com>
2016-05-11 15:54:48 -0400

    do logins

---
+++ app/controllers/users.rb
@@ -0,0 +1,20 @@
+get '/users/new' do
+  @user = User.new
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/show'
+end
