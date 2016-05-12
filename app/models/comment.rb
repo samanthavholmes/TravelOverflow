f6ff766c6c31230f8cad4cf43f756d787a368227
Hanna Taylor & Katherine Broner <pair+hannataylor.katherinebroner@devbootcamp.com>
2016-05-12 16:55:30 -0400

    Fix merge issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :votes, as: :voteable
+
+  def points
+    votes.sum(:value)
+  end
 end
